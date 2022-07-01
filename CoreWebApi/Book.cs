921d70b65fb59c63ed4febcd2b02cbce350dfd62
Khomyn Taras <taraskpiaks@gmail.com>
2022-07-01 23:43:31 +0300

    Add project files.

---
+++ CoreWebApi/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreWebApi
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public int Price { get; set; }
+    }
+}
